8ef2b6364d359c5e8c6b1da747624001d814e18e
kusnadi99 <muh.kusnadi99@gmail.com>
2017-08-25 09:02:30 +0700

    ubah master buat dropdown  karena kekurangan pemanggilan jsnya

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -32,40 +32,51 @@
  	<section id="container" class="">
  		@include('layouts.header.header')
  		@include('layouts.sidebars.sidebar')
+    </section>
  	<section id="main-content">
  	</section>
-
-
-   		{!!Html::script('js/jquery.js')!!}
-  	 	{!!Html::script('js/jquery-ui-1.10.4.min.js')!!}
-     	{!!Html::script('js/jquery-1.8.3.min.js')!!}
-     	{!!Html::script('js/jquery-ui-1.9.2.custom.min.js')!!}
-     	{!!Html::script('js/bootstrap.min.js')!!}
-     	{!!Html::script('jjs/jquery.scrollTo.min.js')!!}
-     	{!!Html::script('js/jquery.nicescroll.js" type="text/javascript')!!}
-     	{!!Html::script('assets/jquery-knob/js/jquery.knob.js')!!}
-     	{!!Html::script('js/jquery.sparkline.js" type="text/javascript')!!}
-     	{!!Html::script('assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js')!!}
-     	{!!Html::script('js/owl.carousel.js')!!}
-     	{!!Html::script('js/fullcalendar.min.js')!!}
-	 	  {!!Html::script('assets/fullcalendar/fullcalendar/fullcalendar.js')!!}
-     	{!!Html::script('js/calendar-custom.js')!!}
-	 	  {!!Html::script('js/jquery.rateit.min.js')!!}
-     	{!!Html::script('js/jquery.customSelect.min.js')!!}
-	 	  {!!Html::script('assets/chart-master/Chart.js')!!}
-     	{!!Html::script('js/sparkline-chart.js')!!}
-     	{!!Html::script('js/easy-pie-chart.js')!!}
-  	 	{!!Html::script('js/jquery-jvectormap-1.2.2.min.js')!!}
-  	 	{!!Html::script('js/jquery-jvectormap-world-mill-en.js')!!}
-  	 	{!!Html::script('js/xcharts.min.js')!!}
-  	 	{!!Html::script('js/jquery.autosize.min.js')!!}
-  		{!!Html::script('js/jquery.placeholder.min.js')!!}
-  		{!!Html::script('js/gdp-data.js')!!}	
-  		{!!Html::script('js/morris.min.js')!!}
-  		{!!Html::script('js/sparklines.js')!!}
-  		{!!Html::script('js/charts.js')!!}
-  		{!!Html::script('js/jquery.slimscroll.min.js')!!}
 		
+      <!-- javascripts -->
+    {!!Html::script('js/jquery.js')!!}
+    {!!Html::script('js/jquery-ui-1.10.4.min.js')!!}
+  {!!Html::script('js/jquery-1.8.3.min.js')!!}
+    {!!Html::script('js/jquery-ui-1.9.2.custom.min.js')!!}
+    <!-- bootstrap -->
+    {!!Html::script('js/bootstrap.min.js')!!}
+    <!-- nice scroll -->
+    {!!Html::script('js/jquery.scrollTo.min.js')!!}
+    {!!Html::script('js/jquery.nicescroll.js')!!}
+    <!-- charts scripts -->
+    {!!Html::script('assets/jquery-knob/js/jquery.knob.js')!!}
+    {!!Html::script('js/jquery.sparkline.js')!!}
+    {!!Html::script('assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js')!!}
+    {!!Html::script('js/owl.carousel.js')!!}
+    <!-- jQuery full calendar -->
+    {!!Html::script('js/fullcalendar.min.js')!!} <!-- Full Google Calendar - Calendar -->
+    {!!Html::script('assets/fullcalendar/fullcalendar/fullcalendar.js')!!}
+    <!--script for this page only-->
+    {!!Html::script('js/calendar-custom.js')!!}
+    {!!Html::script('js/jquery.rateit.min.js')!!}
+    <!-- custom select -->
+    {!!Html::script('js/jquery.customSelect.min.js')!!}
+    {!!Html::script('assets/chart-master/Chart.js')!!}
+   
+    <!--custome script for all page-->
+    {!!Html::script('js/scripts.js')!!}
+    <!-- custom script for this page-->
+    {!!Html::script('js/sparkline-chart.js')!!}
+    {!!Html::script('js/easy-pie-chart.js')!!}
+    {!!Html::script('js/jquery-jvectormap-1.2.2.min.js')!!}
+  {!!Html::script('js/jquery-jvectormap-world-mill-en.js')!!}
+  {!!Html::script('js/xcharts.min.js')!!}
+  {!!Html::script('js/jquery.autosize.min.js')!!}
+  {!!Html::script('js/jquery.placeholder.min.js')!!}
+  {!!Html::script('js/gdp-data.js')!!}
+  {!!Html::script('js/morris.min.js')!!}
+  {!!Html::script('js/sparklines.js')!!}  
+  {!!Html::script('js/charts.js')!!}
+  {!!Html::script('js/jquery.slimscroll.min.js')!!}
+
   <script>
 
       //knob
